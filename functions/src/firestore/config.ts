dff98ec7db553e5a589e255b34448692cecf9f17
Rafael Molina <rafael@molina-aquino.com>
2023-06-13 22:49:34 -0400

    feat: move firebase initialize app to single file

---
+++ functions/src/firestore/config.ts
@@ -0,0 +1,7 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase-admin/app";
+
+// Initialize Firebase
+const firebaseApp = initializeApp();
+
+export default firebaseApp;
